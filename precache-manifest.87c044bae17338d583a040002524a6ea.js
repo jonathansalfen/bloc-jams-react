0018545c2891c1f52185ee0bfb5b7e55874201b6
jonathansalfen <jonathan.salfen@gmail.com>
2019-07-15 14:42:57 -0700

    Updates

---
+++ precache-manifest.87c044bae17338d583a040002524a6ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c9a2e1a401a2307e601",
+    "url": "/bloc-jams-react/static/js/runtime~main.0c766afe.js"
+  },
+  {
+    "revision": "3b1538592eebad9d6e6d",
+    "url": "/bloc-jams-react/static/js/main.ac322a59.chunk.js"
+  },
+  {
+    "revision": "e5584c2b0e377e7bee51",
+    "url": "/bloc-jams-react/static/js/2.e985c332.chunk.js"
+  },
+  {
+    "revision": "3b1538592eebad9d6e6d",
+    "url": "/bloc-jams-react/static/css/main.6815d29a.chunk.css"
+  },
+  {
+    "revision": "c9ea1c59f9cb10a9632d5cf13e08cf7c",
+    "url": "/bloc-jams-react/index.html"
+  }
+];
